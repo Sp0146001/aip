870fe073239cb55a6b4d20a34ca212caabf50787
Sp0146001 <sp0146001@gmail.com>
2025-10-31 22:01:50 +0300

    The initial body of program

--- hw25_10_25/main.cpp
+++ hw25_10_25/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using u_t = unsigned;
+size_t gcd(size_t a, size_t  b){
+	return gcd(b, a%b);
+}
+size_t lcm(size_t  a, size_t  b){
+	return a*b/(gcd(a,b));
+}
 
 int main(){
-        std::cout << "Start working II and III" << "\n";
+	size_t  a = 0, b = 0, result_II = 0, result_III = 0;
+	std::cin >> b;
+	while(std::cin >> a){
+		
+	}
+	if(std::cin.eof()){
+		std::cout << result_II << " " << result_III << "\n";
+	} else if(std::cin.fail()){
+		std::cerr << "Input end\n";
+		return 1;
+	}
         return 0;
 }
