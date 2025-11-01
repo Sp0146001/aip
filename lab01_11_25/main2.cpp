cf0e964fb2d0d12d539282ecfa061d28ba96f034
Sp0146001 <sp0146001@gmail.com>
2025-11-01 19:05:37 +0300

    First commit of task for home

---
+++ lab01_11_25/main2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+void output(const int * const * mtx);
+void input(int ** mtx, int r, int c);
+int ** make(int rows, int cols);
+void rm(int r, int ** mtx);
+
+int ** convert(const int * t, size_t n, const size_t * lns, size_t rows);
+int main(){
+	size_t n = 0, rows = 0;
+	std::cin >> n >> rows;
+	if(std::cin.fail()){
+		std::cerr << "Fail input\n";
+		return 1;
+	}
+	int * t = nullptr;
+	size_t * lns = nullptr;
+	int i = 0;
+	while( i < n + rows){
+		if(i < n){
+			std::cin >> t[i];
+			if(std::cin.fail()){
+		                std::cerr << "Fail input\n";
+                		return 1;
+		        }
+		else {
+			std::cin >> lns[i-n];
+                        if(std::cin.fail()){
+                                std::cerr << "Fail input\n";
+                                return 1;
+                        }
+
+		}
+	}
+}
+
+
+int ** convert(const int * t, size_t n, const size_t * lns, size_t rows){
+	int ** result = new int*[rows];
+	size_t counter = 0;
+        for(size_t i = 0; i < rows; ++i){
+                result[i] = new int(lns[i]);
+                for(int j = 0; j < lns[i]; ++j){
+                        result[i][j] = t[counter];
+			counter++;
+                }
+        }
+	return result;
+}
