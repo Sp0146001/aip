c90e8cbe231dbf3739d091de9eda15d7d2aeb5cb
Sp0146001 <sp0146001@gmail.com>
2025-11-01 12:24:52 +0300

    The initial body of program

--- lab01_11_25/main.cpp
+++ lab01_11_25/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+int make(int rows, int cols){
+	
+}
 int main(){
+	int rows = 0, cols = 0;
+	std::cin >> rows >> cols;
+	if(std::cin.fail()){
+		std::cerr << "Fail input\n";
+		return 1;
+	}
+	int ** mtx = nullptr;
+	mtx = make(rows, cols);
 	
 }
