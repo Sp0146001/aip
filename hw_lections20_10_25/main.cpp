b65d9474f2d04740e44be1b9e99cda9450deaa8b
Sp0146001 <sp0146001@gmail.com>
2025-11-06 01:43:15 +0300

    Fix errors

--- hw_lections20_10_25/main.cpp
+++ hw_lections20_10_25/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 using u_t = unsigned;
 u_t max_u(){
 	using namespace std;
@@ -15,15 +16,15 @@ u_t min_u(){
 //Код 0, если все хорошо + вывод характеристики.
 //Рассчитать НОД для всего массива. По желанию - проверка на переполнение беззнаковых, по желанию - если числа знаковые(UB)
 u_t gcd(u_t a, u_t b);
-void make(size_t n);
-void input(size_t n);
+u_t* make(size_t n);
+u_t* input(size_t n, u_t* mtx);
 u_t u_ovrflw(u_t a, u_t b);
-void output_result(u_t * mtx, size_t n);
-void rm(u_t * mtx);
+void output_result(u_t* mtx, size_t n);
+void rm(u_t* mtx);
 
 int main()
 {
-  size_t a = 0;
+  size_t n = 0;
   std::cin >> n;
   u_t * nums = nullptr;
   try{
@@ -31,34 +32,35 @@ int main()
   } catch(const std::bad_alloc &){
     return 2;
   }
-  nums = input(n);
+  nums = input(n, nums);
   if(std::cin.fail()){
     std::cerr << "Fail input\n";
+    rm(nums);
     return 1;
   }
-  output_result(nums);
-  rm(nums)
+  output_result(nums, n);
+  rm(nums);
   return 0;
 }
 
-size_t gcd(u_t a, u_t b){
+u_t gcd(u_t a, u_t b){
   while(b!=0){
-    size_t rem = a % b;
+    u_t rem = a % b;
     a = b;
     b = rem;
   }
   return a;
 }
-void make(size_t n){
+u_t* make(size_t n){
   try{
-     u_t * mtx = new u_t[n];
+     u_t* mtx = new u_t[n];
   } catch(const std::bad_alloc &){
     rm(mtx);
     return 2;
   }
   return mtx;
 }
-void input(size_t n){
+u_t* input(size_t n, u_t* mtx){
   for(size_t i = 0; i < n; ++i){
     std::cin >> mtx[i];
   }
@@ -66,7 +68,7 @@ void input(size_t n){
 }
 u_t u_ovrflw(u_t a, u_t b){ //runtime-error
 	bool p = (min_u() < a) && (a < max_u());
-	p = p && ((min_u() < b) && (b < max_u());
+	p = p && (min_u() < b) && (b < max_u());
 	if(p){
 		return gcd(a,b);
 	}
@@ -74,17 +76,17 @@ u_t u_ovrflw(u_t a, u_t b){ //runtime-error
 	  std::runtime_error("over/underflowing");
 }
 void output_result(u_t * mtx,size_t n){
-  u_t result = 0;
-  for(size_t i = 0; i < n; ++i){
+  u_t result = mtx[0];
+  for(size_t i = 1; i < n; ++i){
     try{
-      result = u_ovrflw(mtx[i], mtx[i+1]);
+      result = u_ovrflw(result, mtx[i]);
     } catch(const std::runtime_error &e){
       std::cerr << e.what() << "\n";
       return 3;
     }
   }
-  std::cout >> result >> "\n";
+  std::cout << result << "\n";
 }
-void rm(u_t mtx){
+void rm(u_t* mtx){
   delete[] mtx;
 }
