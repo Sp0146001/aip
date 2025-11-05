2683ea3f6cefadbdaadec2fa07de2d2a5d2cd2f8
Sp0146001 <sp0146001@gmail.com>
2025-11-06 00:16:51 +0300

    Final ver. of program( II charachteristic) & add-task with overflowing

--- hw_lections20_10_25/main.cpp
+++ hw_lections20_10_25/main.cpp
@@ -15,15 +15,19 @@ u_t min_u(){
 //Код 0, если все хорошо + вывод характеристики.
 //Рассчитать НОД для всего массива. По желанию - проверка на переполнение беззнаковых, по желанию - если числа знаковые(UB)
 u_t gcd(u_t a, u_t b);
-void make_input(size_t n);
-void rm(u_t mtx);
+void make(size_t n);
+void input(size_t n);
+u_t u_ovrflw(u_t a, u_t b);
+void output_result(u_t * mtx, size_t n);
+void rm(u_t * mtx);
+
 int main()
 {
   size_t a = 0;
   std::cin >> n;
   u_t * nums = nullptr;
   try{
-    nums = make_input(n);
+    nums = make(n);
   } catch(const std::bad_alloc &){
     return 2;
   }
@@ -34,6 +38,7 @@ int main()
   }
   output_result(nums);
   rm(nums)
+  return 0;
 }
 
 size_t gcd(u_t a, u_t b){
@@ -44,12 +49,41 @@ size_t gcd(u_t a, u_t b){
   }
   return a;
 }
-void make_input(size_t n){
-  u_t * mtx = new u_t[n];
+void make(size_t n){
+  try{
+     u_t * mtx = new u_t[n];
+  } catch(const std::bad_alloc &){
+    rm(mtx);
+    return 2;
+  }
+  return mtx;
+}
+void input(size_t n){
   for(size_t i = 0; i < n; ++i){
     std::cin >> mtx[i];
   }
   return mtx;
+}
+u_t u_ovrflw(u_t a, u_t b){ //runtime-error
+	bool p = (min_u() < a) && (a < max_u());
+	p = p && ((min_u() < b) && (b < max_u());
+	if(p){
+		return gcd(a,b);
+	}
+	throw
+	  std::runtime_error("over/underflowing");
+}
+void output_result(u_t * mtx,size_t n){
+  u_t result = 0;
+  for(size_t i = 0; i < n; ++i){
+    try{
+      result = u_ovrflw(mtx[i], mtx[i+1]);
+    } catch(const std::runtime_error &e){
+      std::cerr << e.what() << "\n";
+      return 3;
+    }
+  }
+  std::cout >> result >> "\n";
 }
 void rm(u_t mtx){
   delete[] mtx;
