0e349401be9f3bb826b2cdc86d88ed608b527955
Sp0146001 <sp0146001@gmail.com>
2025-11-07 12:29:13 +0300

    Two func of adding lenght of array

--- lab07_11_25/main.cpp
+++ lab07_11_25/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
-int * extend(const int * a, size_t k, size_t d, int filler);
-
+int * extend(const int * a, size_t k, size_t delta, int fill);
+void extend2(int ** ptr_a, size_t k, size_t d, int f);
 int main(){
   
 }
-int * extend(const int * a, size_t k, size_t d, int f){
-  int * b = new int[d];
+int * extend(const int * a, size_t k, size_t delta, int fill){
+  int * b = new int[delta+k];
   for(size_t i = 0; i < k, ++i){
     b[i] = a[i];
   }
-  for(size_t i = k, i < d; ++i){
-    b[i] = f;
+  for(size_t i = k, i < delta+k; ++i){
+    b[i] = fill;
   }
   return b;
 }
+
+void extend2(int ** ptr_a, size_t k, size_t d, int f){
+  int * b = extend(*ptr_a, k, d, f);
+  delete[] *ptr_a;
+  *ptr_a = b;
