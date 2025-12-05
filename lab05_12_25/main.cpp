5064c2469a53a1035887f96ce0014d1a37149a24
Sp0146001 <sp0146001@gmail.com>
2025-12-05 12:39:38 +0300

    Add == & != & namespace for lesson

--- lab05_12_25/main.cpp
+++ lab05_12_25/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
-
-struct p_t { int x, y; };
-struct Idrow {
-  virtual p_t begin() const = 0;
-  virtual p_t next() const = 0;
-  virtual ~Idrow();
-};
-
+namespace top {
+  struct p_t { int x, y; };
+  bool operator==(p_t, p_t);
+  bool operator!=(p_t, p_t);
+  struct f_t { p_t aa, bb; };
+  struct Idrow {
+    virtual p_t begin() const = 0;
+    virtual p_t next() const = 0;
+    virtual ~Idrow();
+  };
+}
 int main() {
+  using namespace top;
+  p_t a{1, 0}, b{0, 1};
+  std::cout << (a==b) << '\n';
+}
+bool top::operator==(p_t a, p_t b) {
+  return a.x == b.x && a.y == b.y;
+}
+bool top::operator!=(p_t a, p_t b) {
+  return !(a==b);
 }
