2b71b4bd5226ae3c63b10ca3ff8b3738a12c46ec
Sp0146001 <sp0146001@gmail.com>
2025-12-12 10:10:04 +0300

    Start lesson 12.12.2025

---
+++ lab12_12_25/main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+namespace topit {
+  struct p_t { int x, y; };
+  bool operator==(p_t, p_t);
+  bool operator!=(p_t, p_t);
+  struct f_t { p_t aa, bb; };
+
+  struct IDraw {
+    virtual ~IDraw() = default;
+    virtual p_t begin() const = 0;
+    virtual p_t next(p_t) const = 0;
+  };
+
+  struct Dot: IDraw {
+    Dot(int x, int y);
+    explicit Dot(p_t dd);
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t d;
+  };
+    struct Vertical: Idrow {
+    p_t start, end;
+    Vertical(p_t down, p_t up) :
+      start(down), end(up)
+    {}
+    p_t begin() const override {
+      return start;
+    }
+    p_t next(p_t prev) const override {
+      if (prev.x < end.x) {
+        return p_t{prev.x, prev.y+1};
+      }
+      throw std::logic_error("end vertical\n");
+    }
+  }
+  struct Horizontal: Idrow {
+    p_t start, end;
+    Horizontal(p_t down, p_t up) :
+      start(down), end(up)
+    {}
+    p_t begin() const override {
+      return start;
+    }
+    p_t next(p_t prev) const override {
+      if (prev.x < end.x) {
+        return p_t{prev.x+1, prev.y};
+      }
+      throw std::logic_error("end horizontal\n");
+    }
+  }
+
+  // Домашнее задание:
+  // - Добавить ещё 2-3 фигуры:
+  //   - Вертикальный отрезок
+  //   - Горизонтальный отрезок
+  //   - Диагональ под 45 заданной длины
+  //   - Придумать свою фигуру
+
+  // расширять заданный массив точками из очередной фигуры
+  // - extend...
+  size_t points(const IDraw& d, p_t** pts, size_t s);
+
+  // найти минимум и максимум по каждой координате среди точек и сформировать фрейм
+  f_t frame(const p_t* pts, size_t s);
+
+  // построить полотно (из фрейма получить количество столбцов и колонок)
+  char * canvas(f_t fr, char fill);
+
+  // координаты точки перевести в координаты в двумерном массиве
+  void paint(char* cnv, f_t fr, p_t p, char fill);
+
+  // вывод двумперного массива на основе размеров, определяемых фреймом
+  void flush(std::ostream& os, const char* cnv, f_t fr);
+}
+int main() {
+  using topit::IDraw;
+  using topit::Dot;
+  using topit::f_t;
+  using topit::p_t;
+  int err = 0;
+  IDraw* shps[3] = {};
+  p_t * pts = nullptr;
+  size_t s = 0;
+  try {
+    shps[0] = new Dot(0, 0);
+    shps[1] = new Dot(5, 7);
+    shps[2] = new Dot(-5, -2);
+    for (size_t i = 0; i < 3; ++i) {
+      s += points(*(shps[i]), &pts, s);
+    }
+    f_t fr = frame(pts, s);
+    char * cnv = canvas(fr, '.');
+    for (size_t i = 0; i < s; ++i) {
+      paint(cnv, fr, pts[i], '#');
+    }
+    flush(std::cout, cnv, fr);
+    delete [] cnv;
+  } catch (...) {
+    err = 2;
+    std::cerr << "Bad drawing\n";
+  }
+  delete [] pts;
+  delete shps[0];
+  delete shps[1];
+  delete shps[2];
+  return err;
+}
+topit::Dot::Dot(p_t dd):
+ IDraw(),
+ d{dd}
+{}
+topit::Dot::Dot(int x, int y):
+ IDraw(),
+ d{x, y}
+{}
+topit::p_t topit::Dot::begin() const {
+  return d;
+}
+topit::p_t topit::Dot::next(p_t prev) const {
+  if (prev != begin()) {
+    throw std::logic_error("bad impl");
+  }
+  return d;
+}
+bool topit::operator==(p_t a, p_t b) {
+  return a.x == b.x && a.y == b.y;
+}
+bool topit::operator!=(p_t a, p_t b) {
+  return !(a == b);
+}
