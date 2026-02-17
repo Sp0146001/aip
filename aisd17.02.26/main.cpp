5b3be6d3e4d1921066bddabe8153a40733862e11
sp0146001 <sp0146001@gmail.com>
2026-02-17 12:32:35 +0300

    Do convert function

--- aisd17.02.26/main.cpp
+++ aisd17.02.26/main.cpp
@@ -1,1 +1,22 @@
-#inlcude <iostream>
+#include <iostream>
+template< class T> struct  List
+{
+  T val;
+  List < T >* next;
+};
+template< class T>
+List< T >* convert(const T* data, size_t s) {
+  List<T>* r = (!s) ? nullptr : new List<T>{data[0], nullptr};
+  List<T>* t = r;
+  try {
+    for (size_t i = 1; i < s; ++i) {
+      List<T>* n = new List<T>{data[i], nullptr};//new - исключение, утечка с предыдущим new
+      t->next = n;
+      t = n;
+    }
+  } catch (...) {
+    throw std::exception("err\n");
+  }
+  return r;
+}
+
