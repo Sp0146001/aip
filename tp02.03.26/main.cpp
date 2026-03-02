bf04927d1e759eabdcd10d45ff40514810e307c9
sp0146001 <sp0146001@gmail.com>
2026-03-02 10:34:28 +0300

    3x begin/val/next/haxNext

--- tp02.03.26/main.cpp
+++ tp02.03.26/main.cpp
@@ -1,4 +1,4 @@
-include <iostream>
+#include <iostream>
 template< class T> struct Vec
 {
   T* data;
@@ -13,3 +13,42 @@ template< class T> struct List {
   T val;
   List<T>* next;
 };
+template< class T> struct ListIt {
+  List<T>* curr;
+};
+/* functions*/
+template< class T>
+VecIt<T> begin(Vec<T> v) {
+  return { v.data, v.s, v.s};
+}
+template< class T>
+T& value(VecIt<T> it) {
+  return it.data[it.i-1];
+}
+template< class T>
+VecIt<T> next(VecIt<T> it) {
+  --it.i;
+  return it;
+}
+template< class T>
+bool Next(VecIt<T> it) {
+  return it.i;
+}
+/*поможет возможно для S1*/
+template< class T>
+ListIt< T > begin(List< T > h) {
+  return {h};
+}
+template< class T>
+T& value(ListIt<T> it) {
+  return it.curr->val;
+}
+template< class T>
+ListIt<T> next(ListIt<T> it) {
+  it.curr = it.curr->next;
+  return it;
+}
+template< class T>
+bool hasNext(ListIt<T> it) {
+  return it.curr;
+}
