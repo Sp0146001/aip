54dbf1246abe4085f316382166e4e4e8388fdaec
sp0146001 <sp0146001@gmail.com>
2026-03-05 22:06:46 +0300

    task has been started anew

--- hometaskNoFakeLoop/main.cpp
+++ hometaskNoFakeLoop/main.cpp
@@ -5,29 +5,29 @@ struct BiList {
     BiList<T>* next;
     BiList<T>* prev;
 };
-template< class T>
-T& value(BiList<T>* head) {
-  return head->val;
-}
-template< class T>
-BiList<T> next(BiList<T> it) {
-  it = it->next;
-  return it;
-}
-template< class T>
-bool hasNext(BiList<T> it) {
-  return it.next;
-}
-template< class T>
-void clear(BiList<T>* h) {
-  while (h) {
-    BiList<T>* n = h->next;
-    delete h->val;
-    delete[] h;
-    h = n;
-  }
-}
-template<typename T>
-BiList<T>* create_node(const T& val) {
-  return new BiList<T>{val, nullptr, nullptr};
-}
+template<class T>
+void append(BiList<T>** head, BiList<T>** tail, const T value);
+
+template<class T>
+void firstAdd(BiList<T>** head, BiList<T>** tail, const T value);
+
+template<class T>
+T popBack(BiList<T>** head, BiList<T>** tail);
+
+template<class T>
+T popFront(BiList<T>** head, BiList<T>** tail);
+
+template<class T>
+void print(BiList<T>* head, BiList<T>* tail);
+
+template<class T>
+void insert(BiList<T>** head, BiList<T>** tail, const T value, BiList<T>* pos);
+
+template<class T>
+T erase(BiList<T>** head, BiList<T>** tail, BiList<T>* pos);
+
+template<class T>
+void clear(BiList<T>** head, BiList<T>** tail);
+
+template<class T>
+void convertation(BiList<T>** head, BiList<T>** tail, const T* a, size_t n);
