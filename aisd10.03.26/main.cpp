dbb35a1f544cafe502811f170323b4d9e19261da
sp0146001 <sp0146001@gmail.com>
2026-03-10 12:30:39 +0300

    no_common

--- aisd10.03.26/main.cpp
+++ aisd10.03.26/main.cpp
@@ -17,8 +17,25 @@ const List<T>* tohead(const List<T>* h) {
   return h;
 }
 template< class T >
-/*задача, првоерить. что указанные списки не имеют общих элеменов*/
+bool hasCommon(const List<T>* h1, const List<T>* h2) {
+  return (tohead(h1) == tohead(h2));
+}
+/*задача, првоерить, что указанные списки не имеют общих элеменов*/
 template < class T>
 bool no_common(const List< const List< T >* >* h) {
-
+  const List<T>* h1 = h;
+  while(h1) {
+    const List<T>* h2 = h->next;
+    while(h2) {
+      if(h1 == h2) {
+        break;
+      }
+      if(hasCommon(h1->val, h2->val)) {
+        return :::;
+      }
+    h2 = h2->next;
+    }
+    h1 = h1->next;
+  }
 }
+
