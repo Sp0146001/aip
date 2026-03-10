2c577c5ebb20e9db267b6755e86407404afab3bc
sp0146001 <sp0146001@gmail.com>
2026-03-10 13:20:54 +0300

    add smth

--- aisd10.03.26/main.cpp
+++ aisd10.03.26/main.cpp
@@ -3,6 +3,10 @@ template<class T> struct List {
   T val;
   List<T>* next, * prev; 
 };
+template<class T> struct MList {
+  T val;
+  MList<T>* next, * prev, * ref; 
+};
 template<class T> struct Vec
 {
   T* data;
@@ -38,4 +42,43 @@ bool no_common(const List< const List< T >* >* h) {
     h1 = h1->next;
   }
 }
-
+/*в списке с дополнительными связями проверить, что все дополнительные связи указывают в тот же список или не ссылаются никуда вовсе*/
+template< class T> 
+bool selfRefed(List<T>* h) {
+  List<T>* it = tohead(h);
+  List<T>* head = it;
+  while(it) {
+    if (it->ref != tohead(it->next)) {
+      return false;
+    }
+    it = it->next;
+  }
+  return true;
+}
+template< class T>
+bool hasRef(const MList<T>* h) {
+  MList<T>* head = tohead(h);
+  while(head) {
+    if(head->ref && head->ref == h) {
+      return true;
+    }
+    head = head->next;
+  }
+  return false;
+}
+/*в спсике с дополнительными связями найти узлы, на которые нет доп ссылок в других узлах. Вернуть список из таких узлов*/
+template< class T>
+List< const MList<T>* >* noRefs(const MList<T>* h) {
+  const MList<T>* head = tohead(h);
+  List< const MList<T>* >* prevM = nullptr;
+  List< const MList<T>* >* hRef = nullptr;
+  while(head) {
+    if (!(hasRef(head))) {
+      List<const MList<T>* >* href
+      href->val = head;
+      href->prev = prevM;
+      href->next = nullptr;
+      head = head->next;
+    }
+  }
+}
