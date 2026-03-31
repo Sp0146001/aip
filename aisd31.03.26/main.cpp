8f1885d98b6178426bd472f1cf7158b236ac3a38
sp0146001 <sp0146001@gmail.com>
2026-03-31 12:52:04 +0300

    Task 1, realization template hash-function

--- aisd31.03.26/main.cpp
+++ aisd31.03.26/main.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+template< class Key, class T, class Hash, class Equal >
+struct HashTable {
+  using Value = std::pair<T, Key>;
+
+  size_t size, capacity;
+  Value** slots;
+};
+
+template< class K, class T, class Hash, class Equal >
+typename HashTable< K, T, Hash, Equal>::Value** find(const HashTable< K, T, Hash, Equal>& table, const K& key) {
+
+}
+
